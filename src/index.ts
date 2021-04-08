8d042b973ee164ff16521da9d8e4f8003dc6d8e6
Daniel Hermon <danielhermonsync@gmail.com>
2021-04-08 16:12:10 +0300

    feat(logger): first implementation using pino (#4)
    Co-authored-by: CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export const cat = {
-  say: 'meow',
+import pino, { LoggerOptions as PinoOptions, Logger } from 'pino';
+export { Logger } from 'pino';
+
+export type LoggerOptions = Pick<PinoOptions, 'enabled' | 'level' | 'prettyPrint' | 'redact'>;
+
+const baseOptions: PinoOptions = { useLevelLabels: true };
+
+const jsLogger = (options?: LoggerOptions): Logger => {
+  const pinoOptions: PinoOptions = { ...baseOptions, ...options };
+  return pino(pinoOptions);
 };
+
+export default jsLogger;
