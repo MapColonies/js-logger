cab10d8c920f091c3e323d84e97c9d0b28ac04a3
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-04-25 09:55:05 +0300

    feat(logger): added support for destination

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import pino, { LoggerOptions as PinoOptions, Logger } from 'pino';
+import pino, { LoggerOptions as PinoOptions, Logger, destination } from 'pino';
 
 type LoggerOptions = Pick<PinoOptions, 'enabled' | 'level' | 'prettyPrint' | 'redact' | 'hooks'>;
 
-const baseOptions: PinoOptions = { useLevelLabels: true };
+const baseOptions: PinoOptions = {
+  formatters: {
+    level(label): Record<string, string> {
+      return { level: label };
+    },
+  },
+};
 
-const jsLogger = (options?: LoggerOptions): Logger => {
+const jsLogger = (options?: LoggerOptions, destination: string | number = 1): Logger => {
   const pinoOptions: PinoOptions = { ...baseOptions, ...options };
-  return pino(pinoOptions);
+  return pino(pinoOptions, pino.destination(destination));
 };
 
 export { Logger } from 'pino';
